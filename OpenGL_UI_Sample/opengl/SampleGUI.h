163912029678df8c2cc6fd1f22bcd1a5330e1e00
catalyst2001 <deryabin1234kirill@gmail.com>
2021-05-01 14:48:16 +0500

    01.05.2021

--- OpenGL_UI_Sample/opengl/SampleGUI.h
+++ OpenGL_UI_Sample/opengl/SampleGUI.h
@@ -22,6 +22,7 @@ struct Color4 { unsigned char r, g, b, a; };
 // Main interface
 //================================================================
 class IBaseGUIElement {
+public:
 	typedef std::vector<IBaseGUIElement> BaseElemsArray;
 	int m_iID;
 	bool m_bTouched;
@@ -30,11 +31,12 @@ class IBaseGUIElement {
 	const char *m_pName;
 	BaseElemsArray m_ChildElems;
 
-public:
 	IBaseGUIElement() {}
 	~IBaseGUIElement() {}
 
-	virtual void Create() = 0;
+	virtual void Create(int x, int y, int width, int height) = 0;
+	virtual void MouseMove(int x, int y) = 0;
+	virtual void MouseClick(int x, int y, int button) = 0;
 	virtual void Shutdown() = 0;
 	virtual void Render() = 0;
 };
@@ -45,20 +47,65 @@ public:
 class CCanvas : public IBaseGUIElement {
 public:
 	CCanvas() {}
+	CCanvas(IBaseGUIElement &elem) {
+		m_ChildElems.push_back(elem);
+	}
 	~CCanvas() {}
 
-	void Create()
+	void Create(int x, int y, int width, int height)
 	{
-
+		m_Rect.x1.x = x;
+		m_Rect.x1.y = y;
+		m_Rect.x2.x = x + width;
+		m_Rect.x2.y = y + height;
 	}
 };
 
-class SampleGUI
+class CSampleGUI : IBaseGUIElement
 {
+	enum mousebuttons_e {
+		MOUSE_LEFT_BUTTON = 0,
+		MOUSE_RIGHT_BUTTON,
+		MOUSE_CENTER_BUTTON,
+		MOUSE_NUM_BUTTONS
+	};
+
+	CSampleGUI() {}
+	~CSampleGUI() {}
+
+	void AddControl(IBaseGUIElement *canvas) { m_ChildElems.push_back(*canvas); }
+	void Create(int x, int y, int width, int height) {}
+	void MouseMove(int x, int y) {}
+	void MouseClick(int x, int y, int button) {}
 
 
+	void Shutdown() {}
+	void Render() {}
+
+	void Event_MouseMove(int x, int y)
+	{
+
+	}
+
+	void Event_MoveClick(int x, int y, int button)
+	{
+
+	}
+
+	void Event_Keyboard(char ch)
+	{
+
+	}
 
 public:
+	class CInput {
+	public:
+		//CInput() {}
+		//~CInput() {}
+		bool m_Buttons[1024];
+		bool m_Mousebuttons[MOUSE_NUM_BUTTONS];
+	} m_Input;
+
 	//canvas colors
 	Color4 m_CanvasColorBackground;
 	Color4 m_CanvasColorEdge;
