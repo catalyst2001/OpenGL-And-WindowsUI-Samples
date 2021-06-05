09726b3b7d6549c2fed0b45fcd406798535deb70
catalyst2001 <deryabin1234kirill@gmail.com>
2021-06-05 20:44:54 +0500

    05.06.2021 20:44

---
+++ VoxelsTest/voxel/voxel/voxel.cpp
@@ -0,0 +1,179 @@
+﻿#include <stdio.h>
+#include "../../../utilites/glwnd.h"
+#include "../../../utilites/camera.h"
+
+INT Width, Height;
+camera_t camera;
+
+
+#define CHUNK_SIZE 40
+
+float cube[] = {
+	0.f, 0.f, 0.f,
+	0.f, 0.f, 1.f,
+	0.f, 1.f, 1.f,
+	0.f, 1.f, 0.f,
+
+	1.f, 0.f, 0.f,
+	1.f, 0.f, 1.f,
+	1.f, 1.f, 1.f,
+	1.f, 1.f, 0.f,
+
+	0.f, 0.f, 1.f,
+	0.f, 1.f, 1.f,
+	1.f, 1.f, 1.f,
+	1.f, 0.f, 1.f,
+
+	0.f, 0.f, 0.f,
+	0.f, 1.f, 0.f,
+	1.f, 1.f, 0.f,
+	1.f, 0.f, 0.f,
+
+	0.f, 0.f, 0.f,
+	0.f, 0.f, 1.f,
+	1.f, 0.f, 1.f,
+	1.f, 0.f, 0.f,
+
+	0.f, 1.f, 0.f,
+	0.f, 1.f, 1.f,
+	1.f, 1.f, 1.f,
+	1.f, 1.f, 0.f
+};
+
+int indexes[] = {
+	23,
+	1,
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
+	10,
+	11,
+	12,
+	13,
+	14,
+	15,
+	16,
+	17,
+	18,
+	19,
+	20,
+	21,
+	22,
+	0
+};
+
+void fn_draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, Width / (double)Height, 0.1, 1000.0);
+
+	camera_look(&camera);
+
+	//glPointSize(2);
+	//glBegin(GL_POINTS);
+	//for (int y = 0; y < CHUNK_SIZE; y++) {
+	//	for (int x = 0; x < CHUNK_SIZE; x++) {
+	//		for (int z = 0; z < CHUNK_SIZE; z++) {
+	//			glVertex3f((float)x, (float)y, (float)z);
+	//		}
+	//	}
+	//}
+
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, cube);
+	//glDrawArrays(GL_QUADS, 0, 24);
+	glDrawElements(GL_QUADS, (sizeof(cube) / sizeof(float)) / 3, GL_UNSIGNED_INT, indexes);
+	glDisableClientState(GL_VERTEX_ARRAY);
+
+
+	glEnd();
+}
+
+void fn_window_resize(int width, int height)
+{
+	if (!height)
+		height = 1;
+
+	Width = width;
+	Height = height;
+	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, width / (double)height, 0.1, 1000.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	camera_update_viewport(&camera, width, height);
+}
+
+void fn_mousemove(int x, int y)
+{
+}
+
+void fn_mouseclick(int x, int y, int button, int state)
+{
+}
+
+void fn_charinput(char symbol)
+{
+}
+
+//https://docs.microsoft.com/ru-ru/windows/win32/inputdev/wm-keydown
+void fn_keydown(INT state, WPARAM wparam, LPARAM lparam)
+{
+	INT key = (INT)wparam;
+	if (state == KEY_DOWN) {
+		if (key == 27) //esc code
+			exit(0); //close program
+
+		switch (key) {
+		case VK_F1:
+			static bool bpolygon = true;
+			glPolygonMode(GL_FRONT_AND_BACK, bpolygon ? GL_LINE : GL_FILL);
+			bpolygon = !bpolygon;
+			break;
+		}
+	}
+}
+
+//Add this GL functions
+void fn_windowcreate(HWND hWnd)
+{
+	camera_init(&camera, 0.f, 0.f, 5.f, 0.f, 0.f, 0.f);
+	camera_set_active(&camera, true);
+}
+
+void fn_windowclose(HWND hWnd)
+{
+	exit(0);
+}
+
+int main()
+{
+	create_window("Sample OpenGL Application", __FILE__ __TIME__,
+		24,					  //Colors bits
+		32,					  //Depth bits
+		fn_draw,			  //Draw function
+		fn_window_resize,	  //Window resize function
+		fn_mousemove,		  //Mouse move function
+		fn_mouseclick,		  //Mouse click function
+		fn_charinput,		  //Char input function
+		fn_keydown,			  //Keydown function
+		fn_windowcreate,	  //Window create function
+		fn_windowclose,		  //Window close function
+		0,					  //Window position X
+		0,					  //Window position Y
+		1680,				  //Window width
+		1050);				  //Window height
+	return 0;
+}
---
+++ utilites/camera.h
@@ -0,0 +1,96 @@
+#pragma once
+#ifndef __CAMERA_INCLUIDED
+#define __CAMERA_INCLUIDED
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <math.h>
+
+#define FLT_PI 3.141592653
+#define SPEED 0.01
+
+struct camera_t {
+	float startX, startY, startZ;
+	double angleX;
+	double angleY;
+	float endX, endY, endZ;
+
+	POINT mousexy;
+	int width, height;
+	bool active;
+};
+
+inline void camera_init(camera_t *pcamera, float stx, float sty, float stz, float endx, float endy, float endz)
+{
+	pcamera->startX = stx;
+	pcamera->startY = sty;
+	pcamera->startZ = stz;
+	pcamera->endX = endx;
+	pcamera->endY = endy;
+	pcamera->endZ = endz;
+}
+
+inline void camera_update_viewport(camera_t *pcamera, int _width, int _height)
+{
+	pcamera->width = _width;
+	pcamera->height = _height;
+}
+
+inline void camera_set_active(camera_t *pcamera, bool state)
+{
+	pcamera->active = state;
+}
+
+void camera_look(camera_t *pcamera)
+{
+	//update view by mouse
+	if (pcamera->active) {
+		GetCursorPos(&pcamera->mousexy);
+		pcamera->angleX += (pcamera->width / 2 - pcamera->mousexy.x) / 1.1f; //2 — чувствительность
+		pcamera->angleY += (pcamera->height / 2 - pcamera->mousexy.y) / 1.1f;
+		SetCursorPos(pcamera->width / 2, pcamera->height / 2);
+	
+		if(pcamera->angleY < -89.0)
+			pcamera->angleY = -89.0;
+		if(pcamera->angleY > 89.0)
+			pcamera->angleY = 89.0;
+
+		if (GetAsyncKeyState(VK_SHIFT)) {
+			pcamera->startY += (float)SPEED / 2.f;
+		}
+		if (GetAsyncKeyState(VK_CONTROL)) {
+			pcamera->startY -= (float)SPEED / 2.f;
+		}
+		if (GetAsyncKeyState(0x57)) //w
+		{
+			pcamera->startX -= (float)sin(pcamera->angleX / 180.f * FLT_PI) * SPEED;
+			pcamera->startZ -= (float)cos(pcamera->angleX / 180.f * FLT_PI) * SPEED;
+		}
+		if (GetAsyncKeyState(0x53))//s
+		{
+			pcamera->startX += (float)sin(pcamera->angleX / 180.f * FLT_PI) * SPEED;
+			pcamera->startZ += (float)cos(pcamera->angleX / 180.f * FLT_PI) * SPEED;
+		}
+		if (GetAsyncKeyState(0x44)) //right
+		{
+			pcamera->startX += (float)sin((pcamera->angleX + 90) / 180.f * FLT_PI) * SPEED;
+			pcamera->startZ += (float)cos((pcamera->angleX + 90) / 180.f * FLT_PI) * SPEED;
+		}
+		if (GetAsyncKeyState(0x41))//left
+		{
+			pcamera->startX += (float)sin((pcamera->angleX - 90) / 180.f * FLT_PI) * SPEED;
+			pcamera->startZ += (float)cos((pcamera->angleX - 90) / 180.f * FLT_PI) * SPEED;
+		}
+	}
+	
+	pcamera->endX = pcamera->startX - sin(pcamera->angleX / 180.f * FLT_PI);
+	pcamera->endY = pcamera->startY + (tan(pcamera->angleY / 180.f * FLT_PI));
+	pcamera->endZ = pcamera->startZ - cos(pcamera->angleX / 180.f * FLT_PI);
+	//printf("Start: %f %f %f   End: %f %f %f\n", x, y, z, dir[X], dir[Y], dir[Z]);
+
+	//glMatrixMode(GL_MODELVIEW);
+	//glLoadIdentity();
+	gluLookAt(pcamera->startX, pcamera->startY, pcamera->startZ, pcamera->endX, pcamera->endY, pcamera->endZ, 0.f, 1.f, 0.f);
+}
+
+#endif
